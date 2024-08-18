e0581228722c1dbfef06957b45e60f8520128d81
MegaChadDetail <121978004+MegaChadDetail@users.noreply.github.com>
2024-08-18 23:58:54 +0200

    Poprawa działania kodu js

--- skrypt/przeciaganie_slajder.js
+++ skrypt/przeciaganie_slajder.js
@@ -1,38 +1,42 @@
-let isDown = false;
-let startX;
-let scrollLeft;
-const slider = document.querySelector('.zawartosc-urzadzenia');
+document.addEventListener('DOMContentLoaded', () => {
+
+    let isDown = false;
+    let startX;
+    let scrollLeft;
+    const slider = document.querySelector('.zawartosc-urzadzenia');
 
 
-const end = () => {
-	isDown = false;
-  slider.classList.remove('interakcja');
-}
+    const end = () => {
+      isDown = false;
+      slider.classList.remove('interakcja');
+    }
 
-const start = (e) => {
-  isDown = true;
-  slider.classList.add('interakcja');
-  startX = e.pageX || e.touches[0].pageX - slider.offsetLeft;
-  scrollLeft = slider.scrollLeft;	
-}
+    const start = (e) => {
+      isDown = true;
+      slider.classList.add('interakcja');
+      startX = e.pageX || e.touches[0].pageX - slider.offsetLeft;
+      scrollLeft = slider.scrollLeft;	
+    }
 
-const move = (e) => {
-	if(!isDown) return;
+    const move = (e) => {
+      if(!isDown) return;
 
-  e.preventDefault();
-  const x = e.pageX || e.touches[0].pageX - slider.offsetLeft;
-  const dist = (x - startX);
-  slider.scrollLeft = scrollLeft - dist;
-}
+      e.preventDefault();
+      const x = e.pageX || e.touches[0].pageX - slider.offsetLeft;
+      const dist = (x - startX);
+      slider.scrollLeft = scrollLeft - dist;
+    }
 
-(() => {
-	slider.addEventListener('mousedown', start);
-	slider.addEventListener('touchstart', start);
+    (() => {
+      slider.addEventListener('mousedown', start);
+      slider.addEventListener('touchstart', start);
 
-	slider.addEventListener('mousemove', move);
-	slider.addEventListener('touchmove', move);
+      slider.addEventListener('mousemove', move);
+      slider.addEventListener('touchmove', move);
 
-	slider.addEventListener('mouseleave', end);
-	slider.addEventListener('mouseup', end);
-	slider.addEventListener('touchend', end);
-})();
+      slider.addEventListener('mouseleave', end);
+      slider.addEventListener('mouseup', end);
+      slider.addEventListener('touchend', end);
+    })();
+    
+});
