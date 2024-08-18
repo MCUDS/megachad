5b61efaf0364cb06bf34d3ef6774b687d410d864
MegaChadDetail <121978004+MegaChadDetail@users.noreply.github.com>
2024-08-19 00:09:12 +0200

    Poprawa kodu

--- skrypt/slide.js
+++ skrypt/slide.js
@@ -70,41 +70,5 @@ document.addEventListener('DOMContentLoaded', () => {
         const scrollAmount = targetSlide.offsetLeft;
         document.querySelector('.slider').scrollTo({ left: scrollAmount, behavior: "smooth" });
       });
-    });
-    // let isDown = false;
-    // let startX;
-    // let scrollLeft;
-    // const slider = document.querySelector('.slider');
-    
-    // const end = () => {
-    //   isDown = false;
-    //   slider.classList.remove('interakcja');
-    // }
-    
-    // const start = (e) => {
-    //   isDown = true;
-    //   slider.classList.add('interakcja');
-    //   startX = e.pageX || e.touches[0].pageX - slider.offsetLeft;
-    //   scrollLeft = slider.scrollLeft;	
-    // }
-    
-    // const move = (e) => {
-    //   if(!isDown) return;
-    
-    //   e.preventDefault();
-    //   const x = e.pageX || e.touches[0].pageX - slider.offsetLeft;
-    //   const dist = (x - startX);
-    //   slider.scrollLeft = scrollLeft - dist;
-    // }
-    
-    // (() => {
-    //   slider.addEventListener('mousedown', start);
-    
-    //   slider.addEventListener('mousemove', move);
-    
-    //   slider.addEventListener('mouseleave', end);
-    //   slider.addEventListener('mouseup', end);
-    //   slider.addEventListener('touchend', end);
-    // })();
-    
+});
 });
